ce3df9e94be7f2c97a17f3bb159191ab753cee72
Hayk <haykhakobyan95@gmail.com>
2020-08-07 21:26:06 +0400

    added vr without events

---
+++ Assets/SterringWheel.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SterringWheel : MonoBehaviour
+{
+
+    public Transform frontLeftWheelWrapper;
+    public float maxWheelTurnAngle = 540f;
+    private float steerAngle = 0f;
+    private float rotation = 0f;
+    public float rotationSpeed = 10f;
+    private bool left = false;
+    private bool right = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (steerAngle > 0)
+        {
+
+            if (rotation <= 160f)
+            {
+                rotation += Time.deltaTime * rotationSpeed;
+            }
+            frontLeftWheelWrapper.localRotation = Quaternion.Euler(0, 0, -rotation);
+
+
+        }
+        if (steerAngle < 0)
+        {
+
+            if (rotation >= -160f)
+            {
+                rotation -= Time.deltaTime * rotationSpeed;
+
+                frontLeftWheelWrapper.localRotation = Quaternion.Euler(0, 0, -rotation);
+            }
+        }
+
+        if (steerAngle == 0)
+        {
+
+
+            rotation = Time.deltaTime * rotationSpeed;
+            frontLeftWheelWrapper.localRotation = Quaternion.Euler(0, 0, 0);
+
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+
+        steerAngle = maxWheelTurnAngle * SimpleInput.GetAxis("Horizontal");
+    }
+
+
+    //  private float x;
+    // private float z;
+    // private bool rotateX;
+    // private float rotationSpeed;
+
+    // void Start()
+    // {
+    //     x = 0.0f;
+    //     z = 0.0f;
+    //     rotateX = true;
+    //     rotationSpeed = 75.0f;
+    // }
+
+    // void FixedUpdate()
+    // {
+    //     if (rotateX == true)
+    //     {
+    //         x += Time.deltaTime * rotationSpeed;
+
+    //         if (x > 360.0f)
+    //         {
+    //             x = 0.0f;
+    //             rotateX = false;
+    //         }
+    //     }
+    //     else
+    //     {
+    //         z += Time.deltaTime * rotationSpeed;
+
+    //         if (z > 360.0f)
+    //         {
+    //             z = 0.0f;
+    //             rotateX = true;
+    //         }
+    //     }
+
+    //     transform.localRotation = Quaternion.Euler(x, 0, z);
+    // }
+}
